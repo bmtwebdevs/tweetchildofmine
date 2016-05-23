65783175410418d57ce557dd3a2e340dd27642fd
conradj <conradsjackson@gmail.com>
2016-05-23 11:27:58 +0100

    add https://github.com/jesusgollonet/clmtrackr

--- recognizerator/main.js
+++ recognizerator/main.js
@@ -1,3 +1,8 @@
+var clmtrackr = require('./js/index.js')
+var model = require('./models/model_pca_20_svm.json');
+
+var tracker = new clmtrackr.tracker();
+console.log(tracker);
 // get image(s)
 
 // process through emotion engine
