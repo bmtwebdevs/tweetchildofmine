3e92cb45a3e913b7ce65a4e2f0386a2bc3d728b3
Doug McDonald <dougajmcdonald@gmail.com>
2016-05-23 12:43:49 +0100

    Add csv line by line

--- textanalyser/src/tcom.js
+++ textanalyser/src/tcom.js
@@ -1,14 +1,46 @@
 import sentiment from 'sentiment'
-//var csv = require('csv-parse');
+import parse from 'csv-parse';
+import fs from 'fs';
+import async from 'async';
 
-
-export class textAnalyser {
+export class TextAnalyser {
 	
+	writeLine(line) {				
+		console.log(line)
+	}
 	
-	
-	
+	parseFile(file) {
+		
+		fs.createReadStream(file)
+			.pipe(parse({delimiter: ','})
+			.on('data', (csvrow) => {
+        		console.log(csvrow);
+        		//do something with csvrow
+        		//csvData.push(csvrow);        
+    		})							
+		);				
+	}
+				
 }
 
-var r1 = sentiment('great awesome winning at life stupid sick fool');
-console.log(r1);        // Score: -2, Comparative: -0.666
+var analyser = new TextAnalyser();
+
+analyser.parseFile('data/data.csv');
+
+//var r1 = sentiment('great awesome winning at life stupid sick fool');
+//console.log(r1);        // Score: -2, Comparative: -0.666
+
+
+// var inputFile='myfile.csv';
+// 
+// var parser = parse({delimiter: ','}, function (err, data) {
+//   async.eachSeries(data, function (line, callback) {
+//     // do something with the line
+//     doSomething(line).then(function() {
+//       // when processing finishes invoke the callback to move to the next one
+//       callback();
+//     });
+//   }
+// }
+
 
