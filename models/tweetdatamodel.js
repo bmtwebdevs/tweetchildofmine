b34947469bfe4b76760b57f5c6082efd14aa15f3
matt <mattcobley1@gmail.com>
2016-05-24 10:07:46 +0100

    scores added to data model and more work done on twitter service

--- models/tweetdatamodel.js
+++ models/tweetdatamodel.js
@@ -1,5 +1,8 @@
+"use strict";
 var tweetdatamodel = (function () {
     function tweetdatamodel() {
     }
     return tweetdatamodel;
 }());
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = tweetdatamodel;
