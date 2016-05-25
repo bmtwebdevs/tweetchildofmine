a84b16a2f1a30a2ef598b613b519c6b22f41423d
Doug McDonald <dougajmcdonald@gmail.com>
2016-05-25 09:50:27 +0100

    add get-search

--- server/lib/server.js
+++ server/lib/server.js
@@ -32,53 +32,77 @@ var _textanalyser = require('../../textanalyser/textanalyser');
 
 var TextAnalyser = _interopRequireWildcard(_textanalyser);
 
+var _face = require('../../recognizerator/face.js');
+
+var _face2 = _interopRequireDefault(_face);
+
+var _twitter = require('twitter');
+
+var _twitter2 = _interopRequireDefault(_twitter);
+
 function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj.default = obj; return newObj; } }
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-var Face = require('../../recognizerator/face.js');
-var face = new Face();
+var face = new _face2.default();
+
 
 var app = (0, _express2.default)();
 app.server = _http2.default.createServer(app);
 
+var client = new _twitter2.default({
+	consumer_key: 'CKMFkWRso9BXQ68NYR8ODjHHG',
+	consumer_secret: 'x04MYu3bpcz3yt63pJZuTfmxbBuWl7kdm79Mbu7Mh1wSi14Pts',
+	access_token_key: '131168610-cDDxo8FtunKk6cyx7ztN7jQYaI8ztMzhMhfea5k',
+	access_token_secret: 'kaHTr5FxRVYToam6YzbazVfq03ZkuvIvKw4swZXM'
+});
+
 // routes
 app.get('/', function (req, res) {
-
-	_twitterservice2.default.getTweets2(function (tweets) {
-		// for each tweets
-		// 	ProcessingInstruction
-		// 	WScript
-		// 	
-		// goto line 13		
-	});
 
 	res.sendFile(_path2.default.normalize(__dirname + './../../web/index.html'));
 });
 
 app.use(_express2.default.static(_path2.default.normalize(__dirname + './../../web/')));
 
-// app.get('/get-tweets-mock', (req, res) => {
-// 	
-// 	var analyser = new TextAnalyser();		
-// 	
-// 	res.json(analyser.processTweets());
-// 	
-// });
-//
-app.get('/get-tweets', _serverSentEvents2.default, function (req, res) {
+app.get('/tweet-stream', _serverSentEvents2.default, function (req, res) {
 
-	_twitterservice2.default.getTweets2(function (tweets) {
+	var bath = ['51.3758', '-2.3599'];
+	var sanFrancisco = ['-122.75', '36.8', '-121.75', '37.8'];
+	var newYork = ['-74,40', '-73,41'];
+
+	var stream = client.stream('statuses/filter', { track: 'sanFrancisco' });
+
+	stream.on('data', function (tweet) {
+		var processedTweet = JSON.stringify(processTweet(tweet));
+		res.sse('data:' + processedTweet + '\n\n');
+	});
+
+	stream.on('error', function (error) {
+		console.log(error);
+		//res.json(error);
+	});
+});
+
+app.get('/get-tweets', function (req, res) {
+
+	var search = req.query.search;
+
+	var processedTweets = [];
+
+	_twitterservice2.default.getTweets2(search, function (tweets) {
 
 		(0, _lodash2.default)(tweets.statuses).forEach(function (tweet) {
-			var processedTweet = JSON.stringify(processTweet(tweet));
-			res.sse('data: ' + processedTweet + '\n\n');
+			processedTweets.push(processTweet(tweet));
 		});
+
+		res.json(processedTweets);
 	});
 });
 
 function processTweet(tweet) {
 
+	//console.log(tweet);	
 	var tweetModel = {};
 
 	tweetModel.when = tweet.created_at;
@@ -86,20 +110,20 @@ function processTweet(tweet) {
 
 	// text processing
 	tweetModel.textScore = (0, _sentiment2.default)(tweet.text).score;
-	//console.log(tweet.url);
-	// face.analyseMyFaceFromUrl(tweet.url, function(result) {    	
-	// 	console.log(result.statusText, result.emotion);
-	// });
-	//
-	// // face processing
-	// tweetModel.faceScore = 0;
 
-	return tweetModel;
+	// face processing		
+	if (tweet.entities.media && tweet.entities.media[0].url) {
+		tweetModel.url = tweet.entities.media[0].url;
+		return face.analyseMyFaceFromUrl(tweet.entities.media[0].url, function (result) {
+			//console.log(result.statusText, result.emotion);
+
+			tweetModel.faceScore = result.emotion;
+			return tweetModel;
+		});
+	} else {
+		return tweetModel;
+	}
 }
-//
-// app.get('/get-db', (req, res) => {	
-// 		
-// });
 
 // create server
 var port = process.env.PORT || 5000;
